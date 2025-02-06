ee1e71fe4b6360c2c12b1672982dcc5df71e1f8c
andreslopezlu <andreslopezlu@gmail.com>
2025-02-06 12:33:02 -0500

    add subscribe steps selection

--- scripts/subscription-process.js
+++ scripts/subscription-process.js
@@ -1,21 +1,31 @@
-
 const processList = document.querySelector(".process__list")
+const processItems = document.querySelectorAll(".process__item")
 
-const processItems = () => {
-    const items = document.querySelectorAll(".process__item")
-    for (let item of items) {
+const typeList = document.querySelector(".types__list")
+const typeItems = document.querySelectorAll(".type__item")
+
+const quantityList = document.querySelector(".quantity__list")
+const quantityItems = document.querySelectorAll(".quantity__item")
+
+const grindList = document.querySelector(".grind__list")
+const grindItems = document.querySelectorAll(".grind__item")
+
+const deliverList = document.querySelector(".deliver__list")
+const deliverItems = document.querySelectorAll(".deliver__item")
+
+const deselectItems = (itemsList) => {
+    for (let item of itemsList) {
         if (item.classList.contains("selected")) {
             item.classList.toggle("selected")
         }
     }
 }
 
-const applyStyles = () => {
-    const items = document.querySelectorAll(".process__item")
-    for (let item of items) {
+const applyStyles = (itemsList) => {
+    for (let item of itemsList) {
         const title = item.childNodes[1]
         const description = item.childNodes[3]
-        if (item.classList.contains("selected")) {
+        if (item.classList.contains("selected")) { 
             title.style.color = "#FFF"
             description.style.color = "#FFF"
             item.style.backgroundColor = "#0E8784"
@@ -28,10 +38,38 @@ const applyStyles = () => {
 }
 
 const clickProcess = (event) => {
-    processItems()
+    deselectItems(processItems)
     const element = event.target.closest("div")
     element.classList.toggle("selected")
-    applyStyles()
+    applyStyles(processItems)
+}
+const clickType = (event) => {
+    deselectItems(typeItems)
+    const element = event.target.closest("div")
+    element.classList.toggle("selected")
+    applyStyles(typeItems)
+}
+const clickQuantity = (event) => {
+    deselectItems(quantityItems)
+    const element = event.target.closest("div")
+    element.classList.toggle("selected")
+    applyStyles(quantityItems)
+}
+const clickGrind = (event) => {
+    deselectItems(grindItems)
+    const element = event.target.closest("div")
+    element.classList.toggle("selected")
+    applyStyles(grindItems)
+}
+const clickDeliver = (event) => {
+    deselectItems(deliverItems)
+    const element = event.target.closest("div")
+    element.classList.toggle("selected")
+    applyStyles(deliverItems)
 }
 
 processList.addEventListener("click", clickProcess)
+typeList.addEventListener("click", clickType)
+quantityList.addEventListener("click", clickQuantity)
+grindList.addEventListener("click", clickGrind)
+deliverList.addEventListener("click", clickDeliver)
